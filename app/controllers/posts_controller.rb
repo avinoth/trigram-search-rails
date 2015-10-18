a590e2f607a66e35ab6147ac09990039fdf79380
Vinoth <a.vinothk@outlook.com>
2015-10-18 15:28:23 +0530

    A basic page and a basic search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
     if params[:q].present?
+      @posts = Post.where(title: params[:q])
+    else
+      @posts = Post.all
     end
   end
 end
